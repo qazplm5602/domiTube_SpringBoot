2e604ad6b89e7cbbc68b110d300ac0cb86dbde85
qazplm5602 <t01048187045@gmail.com>
2024-03-17 22:08:12 +0900

    헤더 프로필 박스

--- src/frontend/src/Components/Header/Header.tsx
+++ src/frontend/src/Components/Header/Header.tsx
@@ -16,12 +16,13 @@ export default function Header(): JSX.Element {
 
         <Section>
             <Link to="/login" className={style.login}><button>로그인</button></Link>
+            <IconButton className={[style.myProfile]} text='프로필' icon='https://nng-phinf.pstatic.net/MjAyMjA2MTdfNzcg/MDAxNjU1NDYwOTk4MzIx.2GtboKl1AANbxW8mwf7_-3rl1joA5z70GdLSuhVzWssg.ubvmA6JPVkX2fRl0DLLBKY9eBbL2Gh3cN03_MMAwnuAg.PNG/1.png?type=f120_120_na' />
         </Section>
     </header>;
 }
 
-function IconButton({icon, text}: {icon: string, text?: string}): JSX.Element {
-    return <button className={style.iconBtn}>
+function IconButton({icon, text, className}: {icon: string, text?: string, className?: string[]}): JSX.Element {
+    return <button className={[style.iconBtn, ...(className || [])].join(" ")}>
         <img src={icon} />
         {text && <span>{text}</span>}
     </button>;
