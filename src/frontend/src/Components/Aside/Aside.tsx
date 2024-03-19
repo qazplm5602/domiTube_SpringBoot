d33e121eef27ba872d1c1315cb39ee613b9af27d
qazplm5602 <t01048187045@gmail.com>
2024-03-19 09:10:11 +0900

    사이드바 축소 ㅆㄱㄴ

--- src/frontend/src/Components/Aside/Aside.tsx
+++ src/frontend/src/Components/Aside/Aside.tsx
@@ -6,12 +6,17 @@ import homeSvg from './home.svg';
 
 export default function Aside() {
     return <>
-        <DetailSide />
+        <DetailSide fixed={false} />
+        {/* <ShortSide /> */}
     </>;
 }
 
-function DetailSide() {
-    return <aside className={style.side}>
+function DetailSide({fixed}: {fixed: boolean}) {
+    const classList = [style.side];
+    if (fixed)
+        classList.push(style.fixed);
+
+    return <aside className={classList.join(" ")}>
         <Link to="/">
             <Button icon={homeSvg} className={[style.menu, style.active].join(" ")}>홈</Button>
         </Link>
@@ -27,5 +32,17 @@ function DetailSide() {
         <Link to="/">
             <Button icon="https://yt3.ggpht.com/_QGPHrnarLactSDLIisKwBFZ58FiqlaGVJNTznx5KaP75-WLNmmpxCPtgCZNh5us9D-8ZZg5KQ=s88-c-k-c0x00ffffff-no-rj" className={[style.menu, style.channel].join(" ")}>도미임</Button>
         </Link>    
+    </aside>;
+}
+
+function ShortSide() {
+    return <aside className={[style.side, style.short].join(" ")}>
+        <Link to="/">
+            <Button icon={homeSvg} className={[style.menu, style.active].join(" ")}>홈</Button>
+        </Link>
+        
+        <Link to="/">
+            <Button icon={homeSvg} className={style.menu}>구독</Button>
+        </Link>
     </aside>;
 }
