7636553893ecb1aac71deaa89382b60564a7ff0b
qazplm5602 <t01048187045@gmail.com>
2024-03-24 21:16:05 +0900

    비디오플레이어 ㄱㄱㄱ

--- src/frontend/src/Components/Watch/Watch.tsx
+++ src/frontend/src/Components/Watch/Watch.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Section from "../Recycle/Section";
 import style from "./watch.module.css";
+import ReactPlayer from "react-player";
 
 export default function Watch() {
     const { id } = useParams();
@@ -9,6 +10,7 @@ export default function Watch() {
     return <MainLayout className={style.main} sideDisable={true}>
         <Section className={style.video_container}>
             {/* <video> */}
+            <VideoPlayer />
 
             <div className={style.title}>제목인뎅_밍글링</div>
         </Section>
@@ -17,4 +19,16 @@ export default function Watch() {
             밍
         </Section>
     </MainLayout>;
+}
+
+function VideoPlayer() {
+    return <Section className={style.video_player}>
+        <ReactPlayer className={style.player} url="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4" />
+        <div className={style.control_main}>
+            <div className={style.top}></div>
+            <div className={style.bottom}>
+                
+            </div>
+        </div>
+    </Section>;
 }
