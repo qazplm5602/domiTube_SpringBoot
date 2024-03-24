6cdbe2f7defc6b458a023013959379b096a2abb1
domi <domi@domi.kr>
2024-03-24 17:22:30 +0900

    watch 시작

--- src/frontend/src/Components/Watch/Watch.tsx
+++ src/frontend/src/Components/Watch/Watch.tsx
@@ -1,10 +1,20 @@
 import { useParams } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
+import Section from "../Recycle/Section";
+import style from "./watch.module.css";
 
 export default function Watch() {
     const { id } = useParams();
 
-    return <MainLayout sideDisable={true}>
-        watchID: {id}
+    return <MainLayout className={style.main} sideDisable={true}>
+        <Section className={style.video_container}>
+            {/* <video> */}
+
+            <div className={style.title}>제목인뎅_밍글링</div>
+        </Section>
+        
+        <Section className={style.recommand_container}>
+            밍
+        </Section>
     </MainLayout>;
 }
