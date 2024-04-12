65dc77277a68c72fb068286c6e523a474378264f
domi <domi@domi.kr>
2024-04-13 05:04:22 +0900

    로그인 화면

--- src/frontend/src/Components/Login/Login.tsx
+++ src/frontend/src/Components/Login/Login.tsx
@@ -1,7 +1,19 @@
+import style from './login.module.css';
+
 import MainLayout from "../Layout/MainLayout";
+import Input from '../Recycle/Input';
+import { Link } from 'react-router-dom';
+import Button from '../Recycle/Button';
 
 export default function Login() {
-    return <MainLayout sideDisable={true}>
-        LOGIN
+    return <MainLayout className={style.main} sideDisable={true}>
+        <div className={style.title}>domiTube에 로그인</div>
+
+        <Input type="text" title='아이디' className={style.input} />
+        <Input type="password" title='비밀번호' className={style.input} />
+        
+        <div className={style.help}><Link to="/">비밀번호를 잊으셨나요?</Link></div>
+
+        <Button className={style.login}>로그인</Button>
     </MainLayout>;
 }
