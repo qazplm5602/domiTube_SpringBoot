3146b15b4a19efc1b04c789d1db341b3f036dbee
qazplm5602 <t01048187045@gmail.com>
2024-04-15 18:10:54 +0900

    에세스토큰 다시 불러옴 (근데 아직 안함)

--- src/frontend/src/Components/Utils/Fetch.ts
+++ src/frontend/src/Components/Utils/Fetch.ts
@@ -1,7 +1,10 @@
+let processRefresh: Promise<boolean> | undefined;
+
 export interface response {
     code: number,
     data: any
 }
+
 
 export async function request(url: string, option: RequestInit = {}): Promise<response> {
     const accessToken = localStorage.getItem("accessToken");
@@ -14,8 +17,28 @@ export async function request(url: string, option: RequestInit = {}): Promise<re
     }
     
     const response = await fetch(url, option);
+    if (response.status === 401) {
+        const data = await response.json().catch(() => {});
+        if (data !== undefined && data.code === "JWT006") { // refresh 토큰으로 다시 에세스 토큰 받아옴
+            if (processRefresh === undefined) {
+                processRefresh = tokenRefresh();
+            }
+
+            // 대기...
+            if (await processRefresh) {
+                // 다시 호출 ㄱㄱ
+                return await request(url, option);
+            }
+        }
+    }
+    
     return {
         code: response.status,
         data: await response.json().catch(() => undefined)
     };
+}
+
+async function tokenRefresh(): Promise<boolean> {
+    processRefresh = undefined;
+    return true;
 }
