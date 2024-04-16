c83d6632ed702712e24df140b0f374ef77103177
qazplm5602 <t01048187045@gmail.com>
2024-04-16 18:47:46 +0900

    로그인 인풋 value, 이벤트

--- src/frontend/src/Components/Login/Login.tsx
+++ src/frontend/src/Components/Login/Login.tsx
@@ -2,17 +2,33 @@ import style from './login.module.css';
 
 import MainLayout from "../Layout/MainLayout";
 import Input from '../Recycle/Input';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Button from '../Recycle/Button';
+import { useSelector } from 'react-redux';
+import IStore from '../Redux/Type';
+import { useEffect, useState } from 'react';
 
 export default function Login() {
+    const logined = useSelector<IStore>(value => value.login.logined) as boolean;
+    const navigate = useNavigate();
+
+    const [errorT, setErrorT] = useState("");
+    const [id, setId] = useState("");
+    const [password, setPassword] = useState("");
+
+    useEffect(() => {
+        if (logined) // 이미 로그인 중인디???
+            navigate("/"); // 다시 홈으로 가셈
+    }, [logined]);
+
     return <MainLayout className={style.main} sideDisable={true}>
         <div className={style.title}>domiTube에 로그인</div>
 
-        <Input type="text" title='아이디' className={style.input} />
-        <Input type="password" title='비밀번호' className={style.input} />
-        
+        <Input type="text" title='아이디' className={style.input} value={id} onChange={(e) => setId(e.target.value)} />
+        <Input type="password" title='비밀번호' className={style.input} value={password} onChange={e => setPassword(e.target.value)} />
+
         <div className={style.help}><Link to="/">비밀번호를 잊으셨나요?</Link></div>
+        <div className={style.error}>{errorT}</div>
 
         <Button className={style.login}>로그인</Button>
     </MainLayout>;
--- src/frontend/src/Components/Recycle/Input.tsx
+++ src/frontend/src/Components/Recycle/Input.tsx
@@ -1,6 +1,6 @@
-export default function Input({title, className, type, ...props}: {title?: string, className?: string, type: React.HTMLInputTypeAttribute}) {
+export default function Input({title, className, type, onChange, value, ...props}: {title?: string, className?: string, type: React.HTMLInputTypeAttribute, value: string | number | readonly string[], onChange?: React.ChangeEventHandler<HTMLInputElement>}) {
     return <section className={className}>
         {title && <h2>{title}</h2>}
-        <input type={type} {...props} />
+        <input type={type} onChange={onChange} value={value} {...props} />
     </section>;
 }
