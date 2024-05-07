9165f253b70fc5749be4e36c84cdaeb5ba426aa1
qazplm5602 <t01048187045@gmail.com>
2024-05-07 20:30:49 +0900

    사이드바 메뉴중

--- src/frontend/src/Components/Aside/StudioAside.tsx
+++ src/frontend/src/Components/Aside/StudioAside.tsx
@@ -1,7 +1,27 @@
+import { Link } from 'react-router-dom';
+import Section from '../Recycle/Section';
 import style from './studioAside.module.css';
+
+import noProfile from '../../assets/no-profile.png';
+import newtabSvg from './newtab.svg';
+import Button from '../Recycle/Button';
 
 export default function StudioAside() {
     return <aside className={style.main}>
+        <Section className={style.channel}>
+            <Link to="/channel/domi" className={style.icon}>
+                <img src={noProfile} />
+                <div className={style.move}>
+                    <img src={newtabSvg} />
+                </div>
+            </Link>
+            
+            <div className={style.title}>내 채널</div>
+            <div className={style.channel_name}>도미임</div>
+        </Section>
 
+        <Section title="MENU" titleClass={style.menu_title} className={style.menu}>
+            <Button icon={noProfile}>밍</Button>
+        </Section>
     </aside>;
 }
