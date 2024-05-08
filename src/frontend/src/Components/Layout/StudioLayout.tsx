a6b52695bacaac95f1bd335973a93e73b77d7844
qazplm5602 <t01048187045@gmail.com>
2024-05-08 16:45:39 +0900

    스튜디오 대시보드 채널

--- src/frontend/src/Components/Layout/StudioLayout.tsx
+++ src/frontend/src/Components/Layout/StudioLayout.tsx
@@ -3,19 +3,19 @@ import StudioHeader from "../Header/StudioHeader";
 import StudioAside from "../Aside/StudioAside";
 import StudioHome from "../Studio/Home/Home";
 
+import style from './studioLayout.module.css';
+
 export default function StudioLayout() {
     return <>
         <StudioHeader />
 
-        <section style={{ display: "flex", flexDirection: "row", width: "100%", height: 0, flexGrow: 1 }}>
+        <section className={style.content}>
             <StudioAside />
             
-            <main style={{ flexGrow: 1, overflow: "auto", boxSizing: "border-box", padding: 20 }}>
-                <Routes>
-                    <Route path="/" element={<StudioHome />} />
-                    <Route path="/domi" element={<div>메인 아님니다. (도미)</div>} />
-                </Routes>
-            </main>
+            <Routes>
+                <Route path="/" element={<StudioHome />} />
+                <Route path="/domi" element={<div>메인 아님니다. (도미)</div>} />
+            </Routes>
         </section>
     </>;
 }
--- src/frontend/src/Components/Studio/Home/Home.tsx
+++ src/frontend/src/Components/Studio/Home/Home.tsx
@@ -1,3 +1,37 @@
+import Button from '../../Recycle/Button';
+import Section from '../../Recycle/Section';
+import style from './home.module.css';
+
 export default function StudioHome() {
-    return <div>홈</div>;
+    return <main>
+        <h2 className={style.title}>안녕하세요, <span>도미</span>님</h2>
+
+        <ChannelStatus />
+
+        <Section className={style.box_container}>
+    
+        </Section>
+    </main>;
+}
+
+function ChannelStatus() {
+    return <Section className={[style.box_container, style.channel].join(" ")}>
+        <h2>채널 상태</h2>
+        
+        <Section title="구독자 집계" titleClass={style.box_title} className={style.subscribe}>
+            <div>
+                <span className={style.count}>5,859,638</span>
+                <span className={style.count_sub}>명</span>
+            </div>
+            
+            <Button className={style.show_btn}>모두 보기</Button>
+        </Section>
+
+        <Section title="시청자 집계" titleClass={style.box_title} className={style.subscribe}>
+            <div>
+                <span className={style.count}>8,999,999,999</span>
+                <span className={style.count_sub}>명</span>
+            </div>
+        </Section>
+    </Section>;
 }
