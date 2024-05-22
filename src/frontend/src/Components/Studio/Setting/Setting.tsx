410bc0c5720e47408d84fd9181f3247a55333293
qazplm5602 <t01048187045@gmail.com>
2024-05-22 12:06:44 +0900

    설정 변경사항 체크

--- src/frontend/src/Components/Studio/Setting/Setting.tsx
+++ src/frontend/src/Components/Studio/Setting/Setting.tsx
@@ -4,6 +4,10 @@ import style from './setting.module.css';
 
 import noProfile from '../../../assets/no-profile.png';
 import { useEffect, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
+import IStore from '../../Redux/Type';
+import { IloginStore } from '../../Redux/LoginStore';
+import { request } from '../../Utils/Fetch';
 
 interface settingType {
     name: string,
@@ -12,94 +16,113 @@ interface settingType {
 }
 
 export default function StudioSetting() {
-    const [loading, setLoading] = useState(false);
-    const origin = useRef<settingType>();
+    const user = useSelector<IStore, IloginStore>(value => value.login);
+    const banner = useRef(false);
 
-    const iconFile = useRef<File | null>();
-    const bannerFile = useRef<File | null>();
+    const [loading, setLoading] = useState(true);
+    const [changes, setChanges] = useState(false);
+    const [name, setName] = useState("");
 
-    const [iconURL, setIconURL] = useState<string>(noProfile);
-    const [bannerURL, setBannerURL] = useState<string>(noProfile);
+    const iconFile = useRef<File | boolean /* File: 이걸로 바꿀꺼임 / false: 바뀐게 없음 / true: 기본값으로 바꿈.. */>(false);
+    const bannerFile = useRef<File | boolean>(false);
 
-    const changedFile = function(mode: number, e: React.ChangeEvent<HTMLInputElement>) {
-        if (e.currentTarget.files === null) return;
-
-        const file = e.currentTarget.files[0];
+    const checkChanges = function() {
+        let change = false;
         
-        // 체크...ㅇ
+        if (name !== user.name) change = true;
+        if (iconFile.current instanceof File || (user.image !== false && iconFile.current === true)) change = true;
+        if (bannerFile.current instanceof File || (banner.current !== false && bannerFile.current === true)) change = true;
 
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-
-        // reader.result;
-
-        reader.onload = function() {
-            if (mode === 0) {
-                setIconURL(reader.result as string);
-            } else if (mode === 1) {
-                setBannerURL(reader.result as string);
-            }
-        }
-
+        setChanges(change);
+    }
+    
+    const getInfo = async function() {
+        const { code, data } = await request(`/api/studio/setting/banner`);
+        if (code !== 200) return;
         
-        if (mode === 0) {
-            iconFile.current = file;
-        } else if (mode === 1) {
-            bannerFile.current = file;
-        }
+        banner.current = data;
+        setLoading(false);
     }
 
     useEffect(() => {
-        origin.current = {
-            name: "test",
-            icon: false,
-            banner: false,
-        };
-    }, []);
+        // origin.current = {
+        //     name: "test",
+        //     icon: false,
+        //     banner: false,
+        // };
+        if (!user.logined) return;
+
+        getInfo();
+        setName(user.name || "");
+    }, [user]);
+
+    useEffect(checkChanges, [name]);
+
+    if (loading) return null; // 로딩중일때는 안그림
 
     return <main>
         <Section className={style.header}>
             <h2 className={style.title}>설정</h2>
             
             <div className={style.right}>
-                <div>변경사항이 있습니다.</div>
-                <Button>변경사항 저장</Button>
+                {changes && <div>변경사항이 있습니다.</div>}
+                <Button disabled={!changes}>변경사항 저장</Button>
             </div>
         </Section>
 
-        <Input />
+        <Input value={name} onChange={e => setName(e)} />
 
-        <ImageUpload image={iconURL} fileChanged={(e) => changedFile(0, e)} title="아이콘" width={100} height={100} />
-        <ImageUpload image={bannerURL} fileChanged={(e) => changedFile(1, e)} title="배너" width={2560 / 4} height={423 / 4} radius={false} />
-        
+        <ImageUpload defaultImage={user.image === true ? `/api/image/user/${user.id}` : noProfile} emptyImage={noProfile} fileChanged={(e) => {iconFile.current = e; checkChanges()}} title="아이콘" width={100} height={100} />
+        <ImageUpload defaultImage={banner.current ? `/api/image/banner/${user.id}` : noProfile} emptyImage={noProfile} fileChanged={(e) => {bannerFile.current = e; checkChanges()}} title="배너" width={2560 / 4} height={423 / 4} radius={false} />
     </main>;
 }
 
-function Input() {
+function Input({value, onChange}: {value: string, onChange: (value: string) => void}) {
     return <Section title="이름" titleClass={style.input_title} className={style.inputBox}>
         <div className={style.sub}>한글, 영문 3 ~ 10자리까지 가능합니다.</div>
-        <input type="text" />
+        <input value={value} onChange={e => onChange(e.currentTarget.value)} type="text" />
         {/* <div className={style.errorT}>이름을 입력해야 합니다.</div> */}
     </Section>
 }
 
-function ImageUpload({title, width, height, radius = true, image, fileChanged}: {title: string, width: number, height: number, radius?: boolean, image: string, fileChanged: React.ChangeEventHandler<HTMLInputElement>}) {
+function ImageUpload({title, width, height, radius = true, defaultImage, emptyImage, fileChanged}: {title: string, width: number, height: number, radius?: boolean, defaultImage: string, emptyImage: string, fileChanged: (file: File | boolean) => void}) {
     const fileRef = useRef<any>();
+    const [imgURL, setImgURL] = useState(defaultImage);
+
     const uploadBtn = function() {
         fileRef.current.click();
     }
 
+    const fileSelected = function(e: React.ChangeEvent<HTMLInputElement>) {
+        if (e.currentTarget.files === null) return;
+
+        const file = e.currentTarget.files[0];
+        
+        const reader = new FileReader();
+        reader.onload = function() {
+            setImgURL(reader.result as string);
+        }
+
+        reader.readAsDataURL(file);
+        fileChanged(file);
+    }
+
+    const setDefault = function() {
+        setImgURL(emptyImage);
+        fileChanged(true);
+    }
+
     return <Section title={`${title} 변경`} titleClass={style.input_title} className={style.iconBox}>
         <div style={{ width, height, borderRadius: radius ? "50%" : 0 }} className={style.icon_container}>
-            <img className={style.icon} src={image} />
+            <img className={style.icon} src={imgURL} />
             <div className={style.hover}></div>
         </div>
 
         <div className={style.interaction}>
             <Button onClick={uploadBtn}>업로드</Button>
-            <Button>삭제</Button>
+            <Button onClick={setDefault}>삭제</Button>
         </div>
 
-        <input type="file" ref={fileRef} onChange={fileChanged} accept="image/jpeg, image/png" style={{display: "none"}} />
+        <input type="file" ref={fileRef} onChange={fileSelected} accept="image/jpeg, image/png" style={{display: "none"}} />
     </Section>
 }
---
+++ src/main/java/com/domi/domitube/Studio/SettingController.java
@@ -0,0 +1,28 @@
+package com.domi.domitube.Studio;
+
+import com.domi.domitube.Repository.Entity.User;
+import com.domi.domitube.Service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/studio/setting")
+public class SettingController {
+    final UserService userService;
+
+    @GetMapping("/baner")
+    boolean GetMyBanner(HttpServletRequest request, HttpServletResponse response) {
+        User user = userService.GetUserForRequest(request);
+        if (user == null) {
+            response.setStatus(401);
+            return false;
+        }
+
+        return user.getBanner();
+    }
+}
